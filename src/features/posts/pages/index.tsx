5423ae05a622aa0e649dcb25864440720becc7da
waonpad <waonpad@gmail.com>
2024-05-15 23:41:44 +0900

    add: うおおおおおおおおおおおおおお

--- src/features/posts/pages/index.tsx
+++ src/features/posts/pages/index.tsx
@@ -1,16 +1,20 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { CreatePost } from "./create-post";
+import { InfiniteAutoLoadPostList } from "./infinite-auto-load-post-list";
 import { InfinitePostList } from "./infinite-post-list";
 import { PostDetail } from "./post-detail";
 import { PostList } from "./post-list";
+import { UpdatePost } from "./update-post";
 
 export const PostsRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<PostList />} />
       <Route path="/infinite" element={<InfinitePostList />} />
+      <Route path="/infinite/auto" element={<InfiniteAutoLoadPostList />} />
       <Route path=":id" element={<PostDetail />} />
       <Route path="create" element={<CreatePost />} />
+      <Route path=":id/update" element={<UpdatePost />} />
       <Route path="*" element={<Navigate to="." />} />
     </Routes>
   );
