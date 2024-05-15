de8916f8aef8e73826a769a019946a89fe5a7f5e
waonpad <waonpad@gmail.com>
2024-05-15 23:40:01 +0900

    add: うおおおおおおおおおおおおおお

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppProvider } from "./providers/app-provider.tsx";
 import { AppRoutes } from "./routes/index.tsx";
+// import "./styles/destyle.css";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById("root")!).render(
