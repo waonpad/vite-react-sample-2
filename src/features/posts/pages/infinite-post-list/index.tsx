a330234ba96c0d08174035d3d71600b8319949db
waonpad <waonpad@gmail.com>
2024-05-15 23:42:18 +0900

    add: うおおおおおおおおおおおおおお

--- src/features/posts/pages/infinite-post-list/index.tsx
+++ src/features/posts/pages/infinite-post-list/index.tsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
 import { useInfinitePosts } from "../../api/get-infinite-posts";
+import { PostList } from "../../components/post-list";
 
 export const InfinitePostList = () => {
   const {
@@ -21,30 +21,16 @@ export const InfinitePostList = () => {
 
   const posts = postsPages.flat();
 
-  const handleClick = () => {
+  const handleLoadMoreButtonClick = () => {
     fetchNextPage();
   };
 
   return (
     <>
-      <button type="button" onClick={handleClick}>
+      <button type="button" onClick={handleLoadMoreButtonClick}>
         load more
       </button>
-      <div>
-        {posts.map((post) => (
-          <div key={post.id}>
-            <Link to={`/posts/${post.id}`}>
-              <p
-                style={{
-                  margin: 0,
-                }}
-              >
-                {post.title}
-              </p>
-            </Link>
-          </div>
-        ))}
-      </div>
+      <PostList posts={posts} />
     </>
   );
 };
