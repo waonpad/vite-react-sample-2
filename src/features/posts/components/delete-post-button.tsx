d3c740f03162bbe27bb901698694799f9e84bb1b
waonpad <waonpad@gmail.com>
2024-05-15 23:41:06 +0900

    add: うおおおおおおおおおおおおおお

---
+++ src/features/posts/components/delete-post-button.tsx
@@ -0,0 +1,40 @@
+import type { ComponentPropsWithoutRef } from "react";
+import type React from "react";
+import { useDeletePostMutation } from "../api/delete-post";
+import type { postSchema } from "../schemas";
+
+/**
+ * このコンポーネントはドメイン知識を持っているが、HTML構造がシンプルなため、propsを受け取ってみる
+ */
+type Props = ComponentPropsWithoutRef<"button"> & {
+  postId: typeof postSchema._type.id;
+};
+
+/**
+ * 投稿を削除する機能だけを提供するボタン \
+ * 削除ボタンはリストアイテムと詳細ページの両方で使われるので、コンポーネントとして切り出した
+ */
+export const DeletePostButton = ({ postId, onClick, children, ...rest }: Props) => {
+  const { mutateAsync } = useDeletePostMutation();
+
+  const handleDeletePostButtonClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    const [deleteResult, error] = await mutateAsync({ params: { id: postId } });
+
+    if (error) {
+      // エラーに応じた処理
+      throw error;
+    }
+
+    // 成功時の処理
+    console.log("Deleted", deleteResult);
+
+    // propsで渡されたonClickを実行する
+    onClick?.(e);
+  };
+
+  return (
+    <button {...rest} onClick={handleDeletePostButtonClick}>
+      {children}
+    </button>
+  );
+};
