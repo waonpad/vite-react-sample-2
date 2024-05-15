f48e6530f468848595b6bd704aa8b5fb99385e05
waonpad <waonpad@gmail.com>
2024-05-15 23:41:20 +0900

    add: うおおおおおおおおおおおおおお

---
+++ src/features/posts/components/post-list.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import { Fragment } from "react/jsx-runtime";
+import type { postSchema } from "../schemas";
+import { DeletePostButton } from "./delete-post-button";
+import { UpdatePostLinkButton } from "./update-post-link-button";
+
+/**
+ * このコンポーネントはドメイン知識を持っていて汎用性が必要なく、HTML構造も複雑なため、propsは必要最小限しか受け取らない
+ */
+type Props = {
+  posts: (typeof postSchema._type)[];
+};
+
+/**
+ * 投稿リストはいろいろな方法で取得されるが、表示するものは同じなので、リストをコンポーネントとして切り出した
+ */
+export const PostList = ({ posts }: Props) => {
+  return (
+    <div>
+      {posts.map((post) => (
+        <Fragment key={post.id}>
+          <div style={{ display: "flex", flexDirection: "column", gap: 4 }}>
+            <Link to={`/posts/${post.id}`}>
+              <p style={{ margin: 0, fontSize: 20 }}>{post.title}</p>
+            </Link>
+            <p style={{ margin: 0 }}>{post.body}</p>
+            <div style={{ display: "flex", gap: 8 }}>
+              <UpdatePostLinkButton postId={post.id}>Update</UpdatePostLinkButton>
+              <DeletePostButton postId={post.id}>Delete</DeletePostButton>
+            </div>
+          </div>
+          {posts.indexOf(post) !== posts.length - 1 && <hr style={{ marginTop: 16 }} />}
+        </Fragment>
+      ))}
+    </div>
+  );
+};
