f498e6cdc5ef28fcd46dc42f46c7f2c19986f47e
waonpad <waonpad@gmail.com>
2024-05-15 23:40:11 +0900

    add: うおおおおおおおおおおおおおお

--- src/components/layouts/main-layout/index.tsx
+++ src/components/layouts/main-layout/index.tsx
@@ -1,10 +1,24 @@
 import type { ReactNode } from "react";
+import { Link } from "react-router-dom";
 
 export const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <div>layout</div>
-      {children}
+      <div style={{ display: "flex", gap: 12 }}>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Posts</Link>
+        <Link to="/posts/infinite">Infinite</Link>
+        <Link to="/posts/infinite/auto">Infinite Auto Load</Link>
+        <Link to="/posts/create">Create Post</Link>
+      </div>
+      <main
+        style={{
+          marginTop: 12,
+        }}
+      >
+        {children}
+      </main>
     </>
   );
 };
